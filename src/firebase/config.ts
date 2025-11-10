af718b56ccb12fce32f0638204fe642e670374a7
DJ Revell <doug.revelljr@gmail.com>
2025-11-10 17:49:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4909354378-652fa",
+  "appId": "1:424163038035:web:89723f5510be4b192d47a6",
+  "apiKey": "AIzaSyAL7BXIx4D9ZOC0BxBbsZHLYPQ5EXaPMmI",
+  "authDomain": "studio-4909354378-652fa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "424163038035"
+};
