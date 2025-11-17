9c5940970ffc285a940e7f9f0d060e2f086d2b19
DJ Revell <doug.revelljr@gmail.com>
2025-11-17 23:56:22 +0000

    the build failed again. Here are the new logs
    starting build "1f079809-

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,11 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700;800&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
